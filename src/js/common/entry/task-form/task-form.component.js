5303c2447b7d3b1933e4cb6453e631061ac36837
Robert Woodall <rdrwoodall@gmail.com>
2018-02-11 14:54:21 -0600

    allow positive durations only

--- src/js/common/entry/task-form/task-form.component.js
+++ src/js/common/entry/task-form/task-form.component.js
@@ -10,7 +10,7 @@ const TaskFormComponent = {
 
     <form ng-submit="$ctrl.addTask()" novalidate>
       <input type="text" name="name" ng-model="$ctrl.task.name" placeholder="name"/>
-      <input type="number" name="duration" ng-model="$ctrl.task.duration" placeholder="in mins..."/>
+      <input type="number" name="duration" min="1" ng-model="$ctrl.task.duration" placeholder="in mins..."/>
       <input type="submit" value="Add"/>
     </form>
 
